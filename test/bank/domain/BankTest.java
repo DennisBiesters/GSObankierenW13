2a38f79efb413fa0b3656ef259bf76bd409bf26a
DennisBiesters1 <d.biesters@student.fontys.nl>
2015-06-01 23:36:30 +0200

    Bank and IBank unittests added

---
+++ test/bank/domain/BankTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank.domain;
+
+import bank.bankieren.Bank;
+import bank.bankieren.IRekening;
+import bank.bankieren.Money;
+import fontys.util.NumberDoesntExistException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dennis
+ */
+public class BankTest {
+
+    /**
+     * Tests for the openRekening method
+     */
+    @Test
+    public void testOpenRekening() {
+        // setup
+        Bank bank = new Bank("Rabobank");
+
+        // add new accounts
+        assertEquals(100000000, bank.openRekening("Dennis", "Geldrop"));
+        assertEquals(100000001, bank.openRekening("Rick", "Eindhoven"));
+
+        // check if new klant is added and information is correct
+        assertEquals("Dennis", bank.getRekening(100000000).getEigenaar().getNaam());
+        assertEquals("Eindhoven", bank.getRekening(100000001).getEigenaar().getPlaats());
+
+        // fail test if input is incorrect
+        assertEquals(-1, bank.openRekening("Dennis", ""));
+        assertEquals(-1, bank.openRekening("", "Geldrop"));
+        assertEquals(-1, bank.openRekening("", ""));
+    }
+
+    /**
+     * Test for the getRekening method
+     */
+    @Test
+    public void testGetRekening() {
+        // setup
+        Bank bank = new Bank("ING");
+        Integer nr = bank.openRekening("Henk", "Nutspeet");
+        IRekening rekening = bank.getRekening(nr);
+
+        // test if rekening exists
+        assertEquals("Henk", rekening.getEigenaar().getNaam());
+        assertEquals("Nutspeet", rekening.getEigenaar().getPlaats());
+
+        // fail test if rekening doesnt exists
+        assertNull(bank.getRekening(1));
+        assertNull(bank.getRekening(100000001));
+    }
+
+    /**
+     * Test for the maakOver method
+     */
+    @Test
+    public void testMaakOver() throws NumberDoesntExistException {
+        // setup
+        Bank bank = new Bank("Tridios");
+        Integer bron = bank.openRekening("Henk", "Zwolle");
+        Integer bestemming = bank.openRekening("Fred", "America");
+
+        // true tests
+        assertTrue(bank.maakOver(bron, bestemming, new Money(1, Money.EURO)));
+        
+        // false tests
+        assertFalse(bank.maakOver(bron, bestemming, new Money(1000000, Money.EURO)));       
+        
+        // fail test
+        try {
+            bank.maakOver(bron, bron, new Money(1, Money.EURO));
+            fail("money transferred to your own account");
+        } catch (RuntimeException exc) {
+        }
+
+        try {
+            bank.maakOver(bron, bestemming, new Money(-1, Money.EURO));
+            fail("negative amount of money transferred");
+        } catch (RuntimeException exc) {
+        }
+
+        try {
+            bank.maakOver(1, bestemming, new Money(1, Money.EURO));
+            fail("money transferred from not existing source");
+        } catch (NumberDoesntExistException exc) {
+        }
+    }
+
+    /**
+     * Test for the getName method
+     */
+    @Test
+    public void testGetName() {
+        // setup
+        Bank bank = new Bank("ABN AMRO");
+
+        // check if name is correct
+        assertEquals("ABN AMRO", bank.getName());
+    }
+}
